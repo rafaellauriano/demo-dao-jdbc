54715a5e3521bb8407120f66e30219d935ff5fca
Rafael Silva Lauriano <rsilvalauriano@gmail.com>
2024-08-18 00:28:47 -0300

    implementação findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Livros");
-		
-		Seller seller = new Seller(2, "Rafael", "rafa@gmail", new Date(), 4000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+	
+	private Connection conn; //atributo objeto conn
+	
+	public SellerDaoJDBC(Connection conn) { //construtor para forçar uma enjeção de dependencia
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -20,9 +33,50 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public Seller findById(Integer id) {
+	public Seller findById(Integer id) {//encontrar o objeto passando o id como parâmetro
 		
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			// se rs.next() for igual a "0", vai retornar nulo
+			if(rs.next()) {
+				
+				//instância do Departamento
+				Department dep = new Department(); 
+				
+				//setando os valores do Depatamento
+				dep.setId(rs.getInt("DepartmentId")); //
+				dep.setName(rs.getString("DepName"));
+				
+				
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				return obj;
+				 
+			}
+			return null; 
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
